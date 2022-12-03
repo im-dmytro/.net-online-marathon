b2492ea80bf9ecc0ff39d4936c8fc154369d10c2
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-03 15:43:50 +0200

    task3

--- sprint01/Class.cs
+++ sprint01/Class.cs
@@ -91,6 +91,110 @@ namespace sprint01
             return distance;
         }
     }
+    class Fraction
+    {
+        readonly int numerator, denominator;
+        public Fraction(int numerator, int denominator)
+        {
 
+            if (numerator < 0 && denominator < 0)
+            {
+                numerator *= -1;
+                denominator *= -1;
+            }
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public static Fraction operator !(Fraction fraction)
+        {
+            return SimplifyFraction(new Fraction(fraction.denominator, fraction.numerator));
+        }
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            return SimplifyFraction(new Fraction(fraction1.numerator * fraction2.numerator,
+                fraction1.denominator * fraction2.denominator));
+        }
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            return SimplifyFraction(new Fraction(fraction1.numerator * fraction2.denominator,
+                fraction1.denominator * fraction2.numerator));
+        }
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+
+            int lcm = Find_LCM(fraction1.denominator, fraction2.denominator);
+            return new Fraction(fraction1.numerator * (lcm / fraction1.denominator) +
+             fraction2.numerator * (lcm / fraction2.denominator), lcm);
+        }
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+
+            int lcm = Find_LCM(fraction1.denominator, fraction2.denominator);
+            return new Fraction(fraction1.numerator * (lcm / fraction1.denominator) -
+                fraction2.numerator * (lcm / fraction2.denominator), lcm);
+        }
+        public static Fraction operator +(Fraction fraction1)
+        {
+
+            return SimplifyFraction(fraction1);
+        }
+        public static Fraction operator -(Fraction fraction1)
+        {
+            return SimplifyFraction(new Fraction(fraction1.numerator * -1, fraction1.denominator));
+        }
+        public static bool operator ==(Fraction fraction1, Fraction fraction2)
+        {
+
+            return fraction1.Equals(fraction2);
+        }
+        public static bool operator !=(Fraction fraction1, Fraction fraction2)
+        {
+            return !fraction1.Equals(fraction2);
+        }
+        public static Fraction SimplifyFraction(Fraction fraction)
+        {
+            int hcf = Find_HCF(fraction.numerator, fraction.denominator);
+            return new Fraction(fraction.numerator / hcf, fraction.denominator / hcf);
+        }
+        public static int Find_HCF(int a, int b)
+        {
+            int g;
+            while (b != 0)
+            {
+                g = b;
+                b = a % b;
+                a = g;
+            }
+            return a;
+        }
+        public static int Find_LCM(int a, int b)
+        {
+
+            int lcm = (a * b) / Find_HCF(a, b);
+            return lcm;
+        }
+        public override string ToString()
+        {
+            var fraction = SimplifyFraction(new Fraction(numerator, denominator));
+
+            string output = $"{fraction.numerator} / ";
+            if (fraction.denominator < 0) output = $"-{output}{-1 * fraction.denominator}";
+            else output = output + fraction.denominator.ToString();
+            return output;
+        }
+        public override bool Equals(object obj)
+        {
+            var item = obj as Fraction;
+            item = SimplifyFraction(item);
+            Fraction comparableFraction = SimplifyFraction(this);
+            return item.numerator == comparableFraction.numerator &&
+                item.denominator == comparableFraction.denominator;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
 
 }
