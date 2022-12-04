93ac2ca08d71040e95f76cc50a33219fb891c24d
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-04 14:29:10 +0200

    made 3d task in sprint02

---
+++ sprint02_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint02_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Science
+    {
+        public virtual void Awards()
+        {
+            Console.WriteLine("We can obtain the Nobel Prize");
+        }
+    }
+    class Mathematics : Science
+    {
+        public override void Awards()
+        {
+            Console.WriteLine("We dont't need any awards, but we can still obtain Able Prize that nobody else can");
+        }
+    }
+    class Physics : Science
+    {
+
+    }
+}
