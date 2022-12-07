5d9029f8d7c90a29561df6b5d3433b1aab51ebef
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-08 00:11:22 +0200

    made and test first task of sprint 3

--- sprint03/sprint03_01/Program.cs
+++ sprint03/sprint03_01/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace sprint03_01
 {
+    class CalcProgram
+    {
+        public delegate double CalcDelegate(double num1, double num2, char operatorSign);
+        public CalcDelegate funcCalc = Calc;
+
+        public static double Calc(double num1, double num2, char operatorSign)
+        {
+            if (num2 == 0) { return 0; }
+            switch (operatorSign)
+            {
+                case '+':
+                    return num1 + num2;
+                   
+                case '-':
+                    return num1 - num2;
+                case '/':
+                    return num1 / num2;
+                case '*':
+                    return num1 * num2;
+                default:
+                    return 0;
+                   
+            }
+        }
+
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-        }
+
+            CalcProgram calc = new CalcProgram();
+            Console.WriteLine(calc.funcCalc(24, 25, '+'));
+            Console.ReadLine();
+            
+  
+    }
     }
 }
