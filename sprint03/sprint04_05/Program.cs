505c8c4112cde240af39995261ea14418058f580
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-08 22:43:13 +0200

    made 5th task of sprint 3

---
+++ sprint03/sprint04_05/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint03_05
+{
+    
+     class EventProgram
+    {
+       public event EventHandler mewo; 
+        public event EventHandler Show;
+        public EventProgram()
+        {
+            Show += Dog;
+            Show += Cat;
+            Show += Mouse;
+            Show += Elephant;
+            Show(this, null);
+        }
+        public static void Dog(object sender, EventArgs e)
+        {
+            Console.WriteLine("Dog");
+        }
+        public static void Cat(object sender, EventArgs e)
+        {
+            Console.WriteLine("Cat");
+        }
+        public static void Mouse(object sender, EventArgs e)
+        {
+            Console.WriteLine("Mouse");
+        }
+        public static void Elephant(object sender, EventArgs e)
+        {
+            Console.WriteLine("Elephant");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventProgram eventProgram = new EventProgram();
+
+            Console.ReadKey();
+        }
+    }
+}
