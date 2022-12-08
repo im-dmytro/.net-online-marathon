2e2941c0d6a152558c84a16dd8df8d8cceb86adf
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-08 18:04:12 +0200

    made task 2 of sprint 3

--- sprint03/sprint03_02/Program.cs
+++ sprint03/sprint03_02/Program.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace sprint03_02
 {
-    class StringExtensions
+    static class StringExtensions
     {
-
+        public static int WordCount(this string value)
+        {
+            var words=value.Split(' ',',','.', '?', '!', '-', ';', ':', ',').ToList();
+            int wordCount = 0;
+            foreach (var item in words)
+            {
+                if (item.Length > 0) { wordCount++; }
+            }
+            return wordCount;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("lkij jnjn-jknj;ijijm#$^^hgnk???:".WordCount());
+            Console.ReadKey();
         }
     }
 }
