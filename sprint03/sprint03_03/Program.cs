9dbcb268cc99ec6d1e1950378c3a06f976141efc
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-08 18:15:17 +0200

    made task 3 of sprint 3

---
+++ sprint03/sprint03_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint03_03
+{
+    class Program
+    {
+        public static List<int> ListWithPositive(List<int> listOfNegPositElem)
+        {
+            return listOfNegPositElem.FindAll(elem => elem > 0 && elem <= 10);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
