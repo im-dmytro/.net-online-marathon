1d3732f1bfe03095520441b9cdca87b846c46e77
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-12 11:47:57 +0200

    created sln for sprint4

---
+++ sprint04/sprint04_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sprint04_01
+{
+    interface ISimmable
+    {
+        void Swim()
+        {
+            Console.WriteLine("I can swim!");
+        }
+    }
+    interface IFlyable
+    {
+        public int MaxHeight { get { return 0; }  }
+        void Fly()
+        {
+            Console.WriteLine($"I can fly at {MaxHeight} meters height");
+        }
+    }
+    interface ISimmeble
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
