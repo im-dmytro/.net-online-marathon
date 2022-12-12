b49864fe47f3a8a9c22b9a905ec3bb0acb83a35d
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-12 15:35:01 +0200

    made 2 task sprint4

---
+++ sprint04/sprint04_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace sprint04_02
+{
+    interface IAnimal
+    {
+        void Voice();
+
+        void Feed();
+        public string Name { get; set; }
+    }
+    class Cat : IAnimal
+    {
+        public string Name { get; set; }
+        public void Voice()
+        {
+            Console.WriteLine("Mew");
+        }
+        public void Feed()
+        {
+            Console.WriteLine("I eat mice");
+        }
+    }
+    class Dog : IAnimal
+    {
+        public string Name { get; set; }
+        public void Voice()
+        {
+            Console.WriteLine("Woof");
+        }
+        public void Feed()
+        {
+            Console.WriteLine("I eat meat");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IAnimal animal = new Cat();
+            Cat animal1 = new Cat();
+            animal.Feed();
+            animal1.Feed();
+        }
+    }
+}
