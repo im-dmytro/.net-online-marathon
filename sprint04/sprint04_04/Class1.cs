1d643104b6e87658f9671cd6e2ee049ef4ffe133
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-13 10:35:25 +0200

    made task 4 without generic comparable

---
+++ sprint04/sprint04_04/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint04_04
+{
+    interface IShape : ICloneable
+    {
+        double Area()
+        {
+            return 0;
+        }
+    }
+    class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double Area()
+        {
+            return Width * Length;
+        }
+        public object Clone() => new Rectangle()
+        {
+            Length = Length,
+            Width = Width
+        };
+
+    }
+    class Room<T> : ICloneable, IComparable where T : class, IShape
+    {
+        public double Height { get; set; }
+        public T Floor { get; set; }
+
+        public double Volume()
+        {
+            return Floor.Area() * Height;
+        }
+
+        public object Clone() => new Room<T>() { Height = Height, Floor = Floor.Clone() as T };
+        public int CompareTo(object other)
+        {
+            var itemShape = other as Room<T>;
+            if (other is null) 
+                throw new ArgumentException("Not correct values");
+            return Floor.Area().CompareTo(itemShape.Floor.Area());
+        }
+    }
+    class RoomComparerByVolume<T> : IComparer<Room<T>> where T : class, IShape
+    {
+        public int Compare(Room<T>? s1, Room<T>? s2)
+        {
+            if (s1 is null || s2 is null)
+                throw new ArgumentException("Not correct values");
+            return s1.Volume().CompareTo(s2.Volume());
+        }
+    }
+    class Trapezoid : IShape
+    {
+        public double Length2 { get; set; }
+        public double Width { get; set; }
+        public double Length1 { get; set; }
+
+        public double Area()
+        {
+            return Length1 * Length2 * 0.5 * Width;
+        }
+
+        public object Clone() => new Trapezoid()
+        {
+            Length1 = Length1,
+            Length2 = Length2,
+            Width = Width
+        };
+
+   
+    }
+}
---
+++ sprint04/sprint04_04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sprint04_04
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trapezoids = new List<Trapezoid>
+            {
+                 new Trapezoid() { Length1 = 10, Length2 = 13, Width = 40 },
+              new Trapezoid() { Length1 = 10, Length2 = 4, Width = 43 },
+              new Trapezoid() { Length1 = 2, Length2 = 13, Width = 10 },
+              new Trapezoid() { Length1 = 4, Length2 = 13, Width = 10 },
+
+             new Trapezoid() { Length1 = 10, Length2 = 6, Width = 13 }
+        };
+            var rooms = new List<Room<Trapezoid>>();
+            foreach (var item in trapezoids)
+            {
+                rooms.Add(new Room<Trapezoid> { Floor = item, Height = 10 });
+            }
+            rooms.Sort();
+            
+            foreach (var room in rooms)
+            {
+                Console.WriteLine($"{room.Floor.Area()}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
