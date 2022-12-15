a76a223aba9fa867ae3055712db2ef2d57c74da0
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-15 23:03:21 +0200

    made tasks of sprint5

---
+++ sprint5/task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task5
+{
+    class MyReverseNotebook
+    {
+        public static Dictionary<string, List<string>> ReverseNotebook(Dictionary<string, string> phonesToNames)
+        {
+            foreach (var item in phonesToNames) 
+                if (String.IsNullOrEmpty(item.Value)) phonesToNames[item.Key] = ""; 
+
+           return 
+               phonesToNames
+                .ToLookup(x => x.Value, x => x.Key)
+                .ToDictionary(x => x.Key, x => x.ToList());
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>() {
+                { null , "Petro" },
+                { "0467654321" , "Ivan" },
+                { "0667654321" , "Petro" },
+                { "0367654321" , "Ivan" },
+                { "0867654321" , "Mark" }};
+            MyReverseNotebook.ReverseNotebook(dictionary);
+
+        }
+    }
+}
---
+++ sprint5/task6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task6
+{
+    class MyReverseNotebook
+    {
+        public static Lookup<string,string> CreateNotebook(Dictionary<string, string> phonesToNames)
+        {
+            foreach (var item in phonesToNames)
+                if (String.IsNullOrEmpty(item.Value)) phonesToNames[item.Key] = "";
+
+            return (Lookup<string,string>)phonesToNames.ToLookup(x => x.Value, x => x.Key);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>() {
+                { null , "Petro" },
+                { "0467654321" , "Ivan" },
+                { "0667654321" , "Petro" },
+                { "0367654321" , "Ivan" },
+                { "0867654321" , "Mark" }};
+            MyReverseNotebook.CreateNotebook(dictionary);
+        }
+
+    }
+}
