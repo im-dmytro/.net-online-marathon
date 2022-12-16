3c6a123be2b5ca43a434bc76018b730fb0b56dfb
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-16 17:19:20 +0200

    made additionl task of sprint 5

---
+++ sprint5/additional_task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace additional_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
